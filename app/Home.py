2c9830465f9913318cd8aac6bd9cbf6b3ec920ae
pikuz4m31 <basnetpk781@gmail.com>
2025-05-01 15:27:01 +0545

    important notice given

--- app/Home.py
+++ app/Home.py
@@ -136,11 +136,9 @@ if st.session_state.page == "Home":
     - Glacier Lake Mapping and Impact
     - Socio-Economic Impact Assessment (Coming Soon!)
     - NLP Sections (Language Prediction, NER Prediction, Sentiment Analysis, Summary Details, Topic Details)
-
-    ---
-    ⚠️ **Important**: If the page is not redirected properly, try refreshing the browser.
     """)
-
+    st.markdown("---")
+    st.warning("⚠️ Important: If the page is not redirected properly, try refreshing the browser.")
 else:
     page_path = PAGES.get(st.session_state.page, None)
     if page_path:
