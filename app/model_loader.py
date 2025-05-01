b644c3aea5af3779de6d9092b1c6b7a159ff446e
pikuz4m31 <basnetpk781@gmail.com>
2025-05-01 13:32:03 +0545

    solving error for spacy

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,11 +1,19 @@
 import spacy
-from spacy.cli import download
+import spacy.cli
+import logging
+import streamlit as st
 
-# Function to load the spaCy model
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Load the spaCy model (global object)
+@st.cache_resource
 def load_spacy_model():
     try:
         nlp = spacy.load("en_core_web_sm")
     except OSError:
-        download("en_core_web_sm")
-        nlp = spacy.load("en_core_web_sm")
+        logging.error("spaCy model 'en_core_web_sm' not found. Installing it now...")
+        spacy.cli.download("en_core_web_sm")  # This will download the model
+        nlp = spacy.load("en_core_web_sm")  # Reload the model after downloading
+        logging.info("spaCy model 'en_core_web_sm' downloaded and loaded successfully.")
     return nlp
